e6fc8faa667d25d0c1a8c9fa3905c5092042f69e
vtmb <46093906+vtmb@users.noreply.github.com>
2023-05-22 09:08:51 +0200

    Update basic-moodle-backup.sh
    mit angaben von php modulen und datenbank version

--- basic-moodle-backup.sh
+++ basic-moodle-backup.sh
@@ -24,6 +24,17 @@ mysqldump --lock-tables -u $db_user -p$db_password $db_name > $backup_dir/moodle
 nice -n 19 ionice -c3 rsync $rsync_options /var/www/moodledata $backup_dir
 nice -n 19 ionice -c3 rsync $rsync_options /var/www/html/moodle $backup_dir
 
+# Sichern der MariaDB-Version
+mariadb_version=$(mysql --version | awk '{print $5}')
+echo "MariaDB Version: $mariadb_version" > $backup_dir/database_info.txt
+
+# Sichern der PHP-Module
+php_modules=$(php -m)
+echo "PHP-Module: $php_modules" >> $backup_dir/database_info.txt
+
+# Sichern der Apache-Konfigurationsdatei
+cp /etc/apache2/apache2.conf $backup_dir/apache2.conf
+
 # Deaktivieren des Wartungsmodus in Moodle
 php /var/www/html/moodle/admin/cli/maintenance.php --disable
 
@@ -32,6 +43,9 @@ end_time=$(date +%s)
 
 # Laufzeit berechnen
 duration=$((end_time - start_time))
+hours=$((duration / 3600))
+minutes=$(( (duration % 3600) / 60 ))
+seconds=$((duration % 60))
 
 # Laufzeit ausgeben
-echo "Das Backup-Skript wurde in $duration Sekunden ausgeführt."
+echo "Das Backup-Skript wurde in $hours Stunden, $minutes Minuten und $seconds Sekunden ausgeführt."
