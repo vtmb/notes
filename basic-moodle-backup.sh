ddc749aad089c0bf187e4b7c0543e2c08400e745
vtmb <46093906+vtmb@users.noreply.github.com>
2023-06-12 11:08:43 +0200

    path now as variables. backup now as archive

--- basic-moodle-backup.sh
+++ basic-moodle-backup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Setzen Sie das Backup-Verzeichnis
-backup_dir="/home/backup"
+backup_dir="DEIN-BACKUP-VERZEICHNIS"
+archive_dir="DEIN-MOODLE-ARCHIV"
+moodle_installation_dir="/var/www/html/moodle"
+moodle_data_dir="/var/www/moodledata"
+php_interpreter="/usr/bin/php"
 
 # Setzen Sie die Datenbankinformationen
-db_user="Ihr_DB_Benutzername"
-db_password="Ihr_DB_Passwort"
-db_name="moodle"
+db_user="DEIN DATENBANK USER"
+db_password="DEIN DATENBANK PASSWORT"
+db_name="DEI DATENBANK NAME"
 
 # Fügen Sie hier weitere Einstellungen für rsync hinzu, falls gewünscht
 rsync_options="-av --delete --bwlimit=5m"
@@ -15,14 +19,17 @@ rsync_options="-av --delete --bwlimit=5m"
 start_time=$(date +%s)
 
 # Aktivieren des Wartungsmodus in Moodle
-php /var/www/html/moodle/admin/cli/maintenance.php --enable
+echo "Wartungsmodus wird aktiviert"
+$php_interpreter $moodle_installation_dir/admin/cli/maintenance.php --enable
 
 # Führen Sie die Datenbanksicherung mit Lock-Tables durch
-mysqldump --lock-tables -u $db_user -p$db_password $db_name > $backup_dir/moodle_db_backup.sql
+echo "Datenbanksicherung wird erzeugt"
+mysqldump --user=$db_user -p$db_password --lock-tables --databases $db_name > $backup_dir/moodle_db_backup.sql
 
 # Führen Sie die Sicherung mit rsync in niedriger Priorität und niedriger I/O-Priorität aus
-nice -n 19 ionice -c3 rsync $rsync_options /var/www/moodledata $backup_dir
-nice -n 19 ionice -c3 rsync $rsync_options /var/www/html/moodle $backup_dir
+echo "Moodle-Installationsverzeichnis und Moodle-data werden per rysnc synchronisiert"
+nice -n 19 ionice -c3 rsync $rsync_options $moodle_data_dir $backup_dir
+nice -n 19 ionice -c3 rsync $rsync_options $moodle_installation_dir $backup_dir
 
 # Sichern der MariaDB-Version
 mariadb_version=$(mysql --version | awk '{print $5}')
@@ -30,13 +37,17 @@ echo "MariaDB Version: $mariadb_version" > $backup_dir/database_info.txt
 
 # Sichern der PHP-Module
 php_modules=$(php -m)
-echo "PHP-Module: $php_modules" >> $backup_dir/database_info.txt
+echo "PHP-Module: $php_modules" >> $backup_dir/php_info.txt
 
 # Sichern der Apache-Konfigurationsdatei
 cp /etc/apache2/apache2.conf $backup_dir/apache2.conf
 
 # Deaktivieren des Wartungsmodus in Moodle
-php /var/www/html/moodle/admin/cli/maintenance.php --disable
+$php_interpreter $moodle_installation_dir/admin/cli/maintenance.php --disable
+
+# Erstellen eines Archivs
+echo "Archiv wird erstellt, Pfad: $archive_dir/latestbackup.tar.gz"
+tar -cpzf $archive_dir/latestbackup.tar.gz -C $backup_dir .
 
 # Endzeit ermitteln
 end_time=$(date +%s)
